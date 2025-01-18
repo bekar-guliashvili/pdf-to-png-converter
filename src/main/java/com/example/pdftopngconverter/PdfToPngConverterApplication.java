45efad23e1bbcf8efdb09f508485befa7c289475
bekar guliashvili <guliashvili85@gmail.com>
2025-01-17 22:36:38 -0500

    added service

---
+++ src/main/java/com/example/pdftopngconverter/PdfToPngConverterApplication.java
@@ -0,0 +1,13 @@
+package com.example.pdftopngconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfToPngConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfToPngConverterApplication.class, args);
+    }
+
+}
